5fc4d4e1d73cc8a9c9136e509a610f01ebb1450b
van Oostrom <aaricia.van-oostrom@capgemini.com>
2017-10-17 10:32:36 +0200

    Added functionality to the add movie button

--- src/main/java/com/capgemini/movies/MyMovieDatabase/controller/MovieController.java
+++ src/main/java/com/capgemini/movies/MyMovieDatabase/controller/MovieController.java
@@ -1,12 +1,30 @@
 package com.capgemini.movies.MyMovieDatabase.controller;
 
+import com.capgemini.movies.MyMovieDatabase.model.Movie;
+import com.capgemini.movies.MyMovieDatabase.repository.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/movies")
 public class MovieController {
 
+    @Autowired
+    MovieRepository movieRepository;
 
+    @RequestMapping(value="", method= RequestMethod.POST)
+    public void add(@RequestBody Movie movie) {
+        movieRepository.save(movie);
+    }
+
+    @RequestMapping(value="",method = RequestMethod.GET)
+    public Iterable<Movie> getAll() {
+        return movieRepository.findAll();
+    }
 
 }
--- src/main/resources/static/js/Movie.js
+++ src/main/resources/static/js/Movie.js
@@ -1,1 +1,38 @@
 var table = $("#table").DataTable();
+
+$("#btnAddMovie").click(function (e) {
+    e.preventDefault();
+    var obj = getObject();
+    $.ajax({
+        url: "/api/movies/",
+        type: "POST",
+        data: JSON.stringify(obj),
+        contentType: "application/json; charset=utf-8",
+        success: function(result) {
+            getAll();
+        },
+        error: function() { }
+    });
+});
+
+function getAll() {
+    $.get("/api/movies/", function (result) {
+        table.clear();
+        for (var i = 0; i < result.length; i++) {
+            table.row.add([ result[i].title,
+                            result[i].watched,
+                            "<a href=\"javascript:del(" + result[i].movieID + ")\"><font color='#ff3385'><i class='fa fa-trash-o' aria-hidden='true'></i></font></a>",
+                            "<a href=\"javascript:edit("+result[i].movieID+")\"><font color='#ff3385'><i class='fa fa-pencil' aria-hidden='true'></i></font></a>"]);
+        }
+        table.draw();
+    });
+}
+
+function getObject() {
+    var obj = {};
+    obj.title = $("#movieName").val();
+    obj.watched =  false;
+    obj.movieID = $("#id").val();
+    return obj;
+}
+
